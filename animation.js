9d1d1a63fc3879e910980c7723956885948ca165
Sandy Co <sandyyklc@gmail.com>
2019-02-15 22:56:12 -0800

    added arrow

--- animation.js
+++ animation.js
@@ -27,3 +27,8 @@ jQuery(function($) {
   $(window).trigger('scroll');
 
 });
+
+$(window).scroll(function(){
+    $(".arrow").css("opacity", 1 - $(window).scrollTop() / 250);
+  //250 is fade pixels
+  });
